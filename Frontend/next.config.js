7637870f9529deb79bf655e6232c7611229872fb
Naval Kumar <navalkumar@Navals-MacBook-Air.local>
2025-05-15 15:44:51 +0530

    fss

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   env: {
     BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:8000',
   },
+
   output: 'standalone',
+
+  // Important: disable assetPrefix to fix loading of _next static files over IP/HTTPS
+  assetPrefix: '',
 };
 
 module.exports = nextConfig;
+
